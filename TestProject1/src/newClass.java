6014279ca322664b22f4da5b72ee1611bc531159
Michel <Michel@192.168.86.40>
2019-06-02 13:37:13 -0400

    added 3 more line

--- TestProject1/src/newClass.java
+++ TestProject1/src/newClass.java
@@ -4,5 +4,10 @@ public class newClass {
 	public void newClass() {
 	System.out.println("newClass just being called");
 	System.out.println("newClass just being called a second time.");
+	
+	System.out.println("newClass just being called a Third time.");
+	System.out.println("newClass just being called a Third time.");
+	System.out.println("newClass just being called a Third time.");
+	
 	}
 }
